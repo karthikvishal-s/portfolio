9cd77990f76fa4df39208ae55485be64bc5daee8
Karthik Vishal <karthikvishal1506@gmail.com>
2025-06-02 22:24:37 +0530

    Added PenSieve links..

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,30 +2,46 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 function Projects() {
   const navigate = useNavigate();
-    const [loaded, setLoaded] = useState(false);
-    
-      useEffect(() => {
-        setLoaded(true);
-      }, []);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
 
   return (
     <div className="min-h-screen bg-beige flex flex-col items-center p-10">
 
-{/* Back Button */}
-<button
+      {/* Back Button */}
+      <button
         onClick={() => navigate('/')}
-        className="absolute top-4 left-4 bg-tealgreen cursor-pointer text-white px-4 py-2 rounded-full shadow-md hover:bg-green-100 transition-all duration-300 z-10 transition-all duration-[1500ms] delay-[600ms] ease-out text-center" // Added z-10
+        className=" text-2xl absolute top-4 left-4 cursor-pointer text-white px-4 py-2 rounded-full shadow-md hover:bg-green-100 transition-all duration-300 z-10 transition-all duration-[1500ms] delay-[600ms] ease-out text-center" // Added z-10
       >
         ←
       </button>
 
-             <div className={`text-7xl text-bold font-visoria text-tealgreen transition-all duration-[1500ms] delay-[300ms] ease-out  ${loaded ? 'transform translate-y-0 opacity-100' : 'transform -translate-y-20 opacity-0'}`}>
-             My Projects</div>
+      <div className={`text-7xl text-bold font-visoria text-tealgreen transition-all duration-[1500ms] delay-[300ms] ease-out  ${loaded ? 'transform translate-y-0 opacity-100' : 'transform -translate-y-20 opacity-0'}`}>
+        My Projects</div>
       <br></br><br></br><br></br><br></br><br></br><br></br>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-20 w-full h-full  max-w-8xl max-h-9xl">
-        
-        {/* Project Card 1 */}
+
+        {/* Pensieve */}
         <div className={`bg-white rounded-2xl shadow-lg p-20 border-gray-700 transition-all duration-[1500ms] delay-[800ms] ease-out  ${loaded ? 'transform translate-x-0 opacity-100' : 'transform -translate-x-20 opacity-0'}`}>
+          <h2 className="text-3xl font-NewElegance mb-4 text-royal-gold">PenSieve -<span className='text-2xl text-blacky'>A FullStack Application</span></h2><br></br>
+          <p className="text-lg text-gray-700 ">
+          <b>A Public Book Repository for Enthusiasts</b><br></br>
+          A full-stack web application built with the MERN stack (MongoDB, Express.js, React.js, Node.js) that allows book lovers to store, organize, and share links to their favorite books. The UI is styled using TailwindCSS for a clean and responsive experience. Designed to be community-driven, this platform enables users to contribute, discover, and interact with curated book collections.
+          </p>
+          <a href="https://github.com/karthikvishal-s/BookStore-MERN" className="mt-4 px-6 py-3 bg-royal-gold text-white font-semibold rounded-full flex items-center gap-2 transition-transform transition-colors duration-200 hover:scale-105 hover:brightness-110">
+            View Source <span>➔</span>
+          </a>
+          <a href="https://pensieve-karthik.vercel.app/" className="mt-4 px-6 py-3 bg-royal-gold text-white font-semibold rounded-full flex items-center gap-2 transition-transform transition-colors duration-200 hover:scale-105 hover:brightness-110">
+            View Site <span>➔</span>
+          </a>
+        </div>
+
+
+        {/* SportsHub */}
+        <div className={`bg-white rounded-2xl shadow-lg p-20 border-gray-700 transition-all duration-[1500ms] delay-[1300ms] ease-out  ${loaded ? 'transform translate-x-0 opacity-100' : 'transform -translate-x-20 opacity-0'}`}>
           <h2 className="text-3xl font-NewElegance mb-4 text-royal-red">SportsHub - <span className='text-2xl text-blacky'>A FrontEnd Application</span></h2><br></br>
           <p className="text-lg text-gray-700">
             A Front-End website created using HTML, CSS, and JavaScript, and hosted with Electron.
@@ -33,11 +49,11 @@ function Projects() {
             We also implemented real-time countdowns for upcoming major events, individual stats,
             and sportsperson comparisons.
           </p>
-          
+
         </div>
 
-        {/* Project Card 2 */}
-        <div className={`bg-white rounded-2xl shadow-lg p-20 border-gray-700 transition-all duration-[1500ms] delay-[1300ms] ease-out  ${loaded ? 'transform translate-x-0 opacity-100' : 'transform -translate-x-20 opacity-0'}`}>
+        {/* UnitEd */}
+        <div className={`bg-white rounded-2xl shadow-lg p-20 border-gray-700 transition-all duration-[1500ms] delay-[1800ms] ease-out  ${loaded ? 'transform translate-x-0 opacity-100' : 'transform -translate-x-20 opacity-0'}`}>
           <h2 className="text-3xl font-NewElegance mb-4 text-royal-blue">UnitEd - <span className='text-2xl text-blacky'> A FullStack Web App</span></h2><br></br>
           <p className="text-lg text-gray-700">
             A Full-Stack web application built using React (Frontend), Firebase (Backend),
@@ -50,8 +66,8 @@ function Projects() {
           </a>
         </div>
 
-        {/* Project Card 3 */}
-        <div className={`bg-white rounded-2xl shadow-lg p-20 border-gray-700 transition-all duration-[1500ms] delay-[1800ms] ease-out  ${loaded ? 'transform translate-x-0 opacity-100' : 'transform -translate-x-20 opacity-0'}`}>
+        {/* DocHelp*/}
+        <div className={`bg-white rounded-2xl shadow-lg p-20 border-gray-700 transition-all duration-[1500ms] delay-[2300ms] ease-out  ${loaded ? 'transform translate-x-0 opacity-100' : 'transform -translate-x-20 opacity-0'}`}>
           <h2 className="text-3xl font-NewElegance mb-4 text-royal-purple">DocHelp -<span className='text-2xl text-blacky'> A FullStack Application</span></h2><br></br>
           <p className="text-lg text-gray-700">
             A full-stack application built using the MERN stack and hosted on Vercel.
@@ -59,41 +75,27 @@ function Projects() {
             Each patient's prescription receipt is printed while securely storing all data in MongoDB.
             (Work in progress.)
           </p>
-          
+
         </div>
 
-        {/* Project Card 4 */}
-        <div className={`bg-white rounded-2xl shadow-lg p-20 border-gray-700 transition-all duration-[1500ms] delay-[2300ms] ease-out  ${loaded ? 'transform translate-x-0 opacity-100' : 'transform -translate-x-20 opacity-0'}`}>
+        {/* Portfolio */}
+        <div className={`bg-white rounded-2xl shadow-lg p-20 border-gray-700 transition-all duration-[1500ms] delay-[2800ms] ease-out  ${loaded ? 'transform translate-x-0 opacity-100' : 'transform -translate-x-20 opacity-0'}`}>
           <h2 className="text-3xl font-NewElegance mb-4 text-royal-gold">Portfolio -<span className='text-2xl text-blacky'>A FrontEnd Application</span></h2><br></br>
           <p className="text-lg text-gray-700">
             A personal portfolio website built using React and TailwindCSS,
-            showcasing various animations and transformations. 
-            Enhanced with multiple React extensions for better interactivity. 
+            showcasing various animations and transformations.
+            Enhanced with multiple React extensions for better interactivity.
             Hosted on Vercel.
           </p>
-          <a  href="https://github.com/karthikvishal-s/portfolio" className="mt-4 px-6 py-3 bg-royal-gold text-white font-semibold rounded-full flex items-center gap-2 transition-transform transition-colors duration-200 hover:scale-105 hover:brightness-110">
+          <a href="https://github.com/karthikvishal-s/portfolio" className="mt-4 px-6 py-3 bg-royal-gold text-white font-semibold rounded-full flex items-center gap-2 transition-transform transition-colors duration-200 hover:scale-105 hover:brightness-110">
             View Source <span>➔</span>
           </a>
-          <a  href="https://portfolio-karthik-18.vercel.app/" className="mt-4 px-6 py-3 bg-royal-gold text-white font-semibold rounded-full flex items-center gap-2 transition-transform transition-colors duration-200 hover:scale-105 hover:brightness-110">
+          <a href="https://portfolio-karthik-18.vercel.app/" className="mt-4 px-6 py-3 bg-royal-gold text-white font-semibold rounded-full flex items-center gap-2 transition-transform transition-colors duration-200 hover:scale-105 hover:brightness-110">
             View Site <span>➔</span>
           </a>
         </div>
 
-        <div className={`bg-white rounded-2xl shadow-lg p-20 border-gray-700 transition-all duration-[1500ms] delay-[2300ms] ease-out  ${loaded ? 'transform translate-x-0 opacity-100' : 'transform -translate-x-20 opacity-0'}`}>
-          <h2 className="text-3xl font-NewElegance mb-4 text-royal-gold">Portfolio -<span className='text-2xl text-blacky'>A FrontEnd Application</span></h2><br></br>
-          <p className="text-lg text-gray-700">
-            A personal portfolio website built using React and TailwindCSS,
-            showcasing various animations and transformations. 
-            Enhanced with multiple React extensions for better interactivity. 
-            Hosted on Vercel.
-          </p>
-          <a  href="https://github.com/karthikvishal-s/portfolio" className="mt-4 px-6 py-3 bg-royal-gold text-white font-semibold rounded-full flex items-center gap-2 transition-transform transition-colors duration-200 hover:scale-105 hover:brightness-110">
-            View Source <span>➔</span>
-          </a>
-          <a  href="https://portfolio-karthik-18.vercel.app/" className="mt-4 px-6 py-3 bg-royal-gold text-white font-semibold rounded-full flex items-center gap-2 transition-transform transition-colors duration-200 hover:scale-105 hover:brightness-110">
-            View Site <span>➔</span>
-          </a>
-        </div>
+
 
       </div>
     </div>
